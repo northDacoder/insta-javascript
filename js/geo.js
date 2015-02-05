6c3b96b5b79f66c7e1c86e11129ccf91ff092b1f
John Ryan <northdacoder@gmail.com>
2015-02-05 12:05:47 -0600

    updated local version

--- js/geo.js
+++ js/geo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+    /* Define 2 variables that will start with no values, once the users geolocation is available
+    these values will be reassigned to the actual latitude & longitude values */
+    var latitude;
+    var longitude;
+
+    Number.prototype.toRad = function() { return this * (Math.PI / 180); };
 
     var dataArr = [];
     var cleanData = [];
@@ -71,7 +77,25 @@ $(document).ready(function(){
 
 
             if (sliceArr[i].location != null) {
+                var imgLat = sliceArr[i].location.latitude;
+                var imgLon = sliceArr[i].location.longitude;
+                var latitudeRad = latitude.toRad();
+                var imgLatRad = imgLat.toRad();
+                var distanceLat = (imgLat - latitude).toRad();
+                var distanceLon = (imgLon - longitude).toRad();
+
+                var R = 6371; // km
+                var a = Math.sin(distanceLat/2) * Math.sin(distanceLat/2) + Math.sin(distanceLon/2) * Math.sin(distanceLon/2) * Math.cos(latitudeRad) * Math.cos(imgLatRad);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                var distanceKilo = R * c;
+                var distanceMiles = Math.round((distanceKilo * 3)/5);
+                imageObj["distance"] = distanceMiles;
                 imageObj["location_name"] = sliceArr[i].location.name;
+
+                cleanData.sort(function(a, b){
+                    return a.distance - b.distance;
+                });
+
             }
 
             cleanData.push(imageObj);
@@ -99,7 +123,36 @@ $(document).ready(function(){
     }
 
 
+    // Success function that will be called if the broswer is able to access the native geolocation functionality
+    // navigator.geolocation.getCurrentPosition(success, error);
+    function success(position) {
+        latitude = Number(position.coords.latitude);
+        longitude = Number(position.coords.longitude);
 
+        /* After successfully loading the latitude & longitude from
+        the users location, we execute the getData() function that will
+        make an Ajax call to Instagram & retrieve the current users data */
+        getData();
+    }
+
+
+    // Error function that will be called if the browser does not have geolocation capabilities & is not supported
+    function error(msg) {
+        console.log(msg);
+    }
+
+
+
+    // If-else statement to ask permission to the user if the app can access their current geolocation coordinates & also see if the functionality exists in the current browser
+    if (navigator.geolocation) {
+        console.log("Geolocation found, getting coordinates now...");
+        navigator.geolocation.getCurrentPosition(success, error);
+        urlarray = window.location.hash.split("=");
+        usertoken = urlarray[1];
+    } else {
+        console.log("Geolocation not found, please try again!");
+        error("not supported");
+    }
 
 
 
